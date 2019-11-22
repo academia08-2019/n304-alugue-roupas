aa1408f43fe14486921269a8e5faf9037c6b5f8a
Ana Beatriz Morita <anabemorita@gmail.com>
2019-11-22 10:44:13 -0300

    criar model de Produto

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    opcoes_tamanho = [('PP', 'Extra pequeno'),
+    ('P', 'Pequeno'),
+    ('M', 'Medio'),
+    ('G', 'Grande'),
+    ('GG', 'Extra grande'),
+    ]
+
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=1000, default=50)
+    disponivel = models.BooleanField(default=True)
+    quantidade = models.IntegerField(default=1)
+    tamanho  = models.CharField(max_length=2, choices=opcoes_tamanho)
